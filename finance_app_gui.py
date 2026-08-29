b632c787fbd27dd31311302ae86041a8ce99a0d2
Justin Lassiter <134980834+lassitup@users.noreply.github.com>
2026-08-29 12:52:59 -0400

    add GUI updates

--- finance_app_gui.py
+++ finance_app_gui.py
@@ -4,15 +4,24 @@ import Database.database_manage as dbm
 
 class App:
     def __init__(self, root):
+
         self.root = root
+        width= self.root.winfo_screenwidth()               
+        height= self.root.winfo_screenheight()               
+        self.root.geometry("%dx%d" % (width, height))
+
+
         self.root.title("Personal Finance Application")
 
-        self.header_lbl = tk.Label(self.root, text="Personal Finance App", width=100, height=2)
-        self.header_lbl.pack()
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
+        self.header_lbl = tk.Label(self.root, text="Personal Finance App", height=2)
+        self.header_lbl.grid(row=0, column=0, sticky="nsew")
 
         # Establish 'main' frame within the window that will house all of the other screen frames
-        self.parent_frame = tk.Frame(self.root, borderwidth=5)
-        self.parent_frame.pack(fill=tk.BOTH,expand=True)
+        self.parent_frame = tk.Frame(self.root)
+        self.parent_frame.grid(row=1, column=0, sticky="nsew")
 
         self.parent_frame.columnconfigure(0, weight=1)
         self.parent_frame.rowconfigure(0, weight=1)
@@ -26,6 +35,7 @@ class App:
 
         # All of the various screens need to be assigned to the same area of the screen so I can 
         # cycle through the screens and all will occupy the same area of the screen
+
         self.welcome_screen.grid(row=0, column=0, sticky="nsew")
         self.transaction_screen.grid(row=0, column=0, sticky="nsew")
 
@@ -47,11 +57,14 @@ class Welcome(tk.Frame):
 
         self.main_app = main_app
 
-        self.welcome_lbl = tk.Label(self, text="Welcome to Personal Finance", width=100, height=20, bg="blue")
-        self.welcome_lbl.pack()
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
-        self.transaction_btn = tk.Button(self, text="see transactions", bg="orange", command=self.main_app.show_transaction_screen)
-        self.transaction_btn.pack()
+        self.welcome_lbl = tk.Label(self, text="Welcome to Personal Finance", height=20, bg="blue")
+        self.welcome_lbl.grid(row=0, column=0, sticky="nsew")
+
+        self.transaction_btn = tk.Button(self, text="See transactions", bg="orange", command=self.main_app.show_transaction_screen)
+        self.transaction_btn.grid(row=1, column=0)
 
 
 class Transactions(tk.Frame):
@@ -61,62 +74,60 @@ class Transactions(tk.Frame):
 
         self.main_app = main_app
 
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
         # This section of code creates a canvas widget and a scrollbar that will allow the user to scroll through the transactions listing if it exceeds the size of the window
         self.canvas = tk.Canvas(self)
 
         # Instantiate the scrollbar objects for each direction
         self.scrollbary = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-
-        self.scrollbarx = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
+        # self.scrollbarx = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
 
         self.content_frame = tk.Frame(self.canvas)
+
+        self.content_frame.columnconfigure(0, weight=1)
+        self.content_frame.rowconfigure(0, weight=1)
 
         self.content_frame.bind("<Configure>", lambda event: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
 
         self.content_window = self.canvas.create_window((0, 0), window=self.content_frame,anchor="nw")
 
         self.canvas.configure(yscrollcommand=self.scrollbary.set)
-        self.canvas.configure(xscrollcommand=self.scrollbarx.set)
+        # self.canvas.configure(xscrollcommand=self.scrollbarx.set)
 
         self.canvas.bind("<Configure>", lambda event: self.canvas.itemconfigure(self.content_window, width=event.width))
+
 
         self.canvas.grid(row=0, column=0, sticky="nsew")
 
         current_cols, current_rows = self.grid_size()
 
         self.scrollbary.grid(row=0, column=1, sticky="ns")
-        self.scrollbarx.grid(row=current_rows, column=0, sticky="ew")
-
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
+        # self.scrollbarx.grid(row=current_rows, column=0, columnspan=current_cols, sticky="ew")
 
 
-
-        transaction_lbl = tk.Label(self.content_frame, text="Transactions", width=100, height=5)
-        transaction_lbl.grid(row=0, column=0)
+        transaction_lbl = tk.Label(self.content_frame, text="Transactions")
+        transaction_lbl.grid(row=0, column=0, sticky="nsew")
 
         self.menu_btn = tk.Button(self.content_frame, text="Main Menu", bg="orange", command=self.main_app.show_welcome_screen)
-        self.menu_btn.grid(row=1, column=0)
+        self.menu_btn.grid(row=1, column=0, sticky="nsew")
 
         self.transaction_btn = tk.Button(self.content_frame, text="Query Transactions", bg="orange", command=self.query_transactions)
-        self.transaction_btn.grid(row=2, column=0)
+        self.transaction_btn.grid(row=2, column=0, sticky="nsew")
 
       
     def query_transactions(self):
         transactions = dbm.get_transactions() 
 
-
-
-
-
-
         # Display the Transactions
-        for row_i in range(3, 3+len(transactions)):
-            # self.rowconfigure(row_i, weight=1)
+        for row_i in range(0, len(transactions)):
+            frame = tk.Frame(self.content_frame)
+            self.rowconfigure(row_i, weight=1)
             for column_i in range(7):
-                # self.columnconfigure(column_i, weight=1)
+                self.columnconfigure(column_i, weight=1)
 
-                frame = tk.Frame(self.content_frame)
+                
                 frame.grid(row=row_i, column=column_i)
                 label = tk.Label(frame, text=f"Row {row_i}, Column {column_i}")
                 label.grid(row=0, column=0)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tkinter as tk
 def main():
     # Create the GUI's main window object here
     root = tk.Tk()
+
     # Instantiates an App object - pass the main window as the parent
     app = App(root)
     # Begin Tkinter's event loop
